2e651e6409260681e0f24885d7676addb0f74cd6
mitsakos178 <76257091+mitsakos178@users.noreply.github.com>
2021-07-20 13:41:37 +0300

    Changes

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Move : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Transform target;
+    public NavMeshAgent agent;
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.destination = target.position;
     }
 }
