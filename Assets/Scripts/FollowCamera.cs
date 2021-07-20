8205ac84c39f823f16fffbf3661dc3218b80c903
mitsakos178 <76257091+mitsakos178@users.noreply.github.com>
2021-07-20 18:38:26 +0300

    S

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class FollowCamera : MonoBehaviour
 {
     [SerializeField] Transform player;
-  
+
+    public float speed = 3.5f;
     
+
     void Update()
     {
         transform.position = player.position;
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            CameraRot();
+        }
+    }
+
+    public void CameraRot()
+    {
+        Debug.Log(Input.GetAxis("Mouse X"));
     }
 }
