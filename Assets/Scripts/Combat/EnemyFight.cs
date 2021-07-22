4302be2e88c34e5c2afe9b61b36e1851ecfe6554
mitsakos178 <76257091+mitsakos178@users.noreply.github.com>
2021-07-22 14:33:41 +0300

    Changes

---
+++ Assets/Scripts/Combat/EnemyFight.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Combat
+{
+
+
+    public class EnemyFight : MonoBehaviour
+    {
+        public void Attack(TargetEnemy target)
+        {
+            print("Hitting");
+        }
+
+
+    }
+}
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,26 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Assets.Scripts.Player.Movement;
+using Assets.Scripts.Combat;
+using System;
+
 namespace Assets.Scripts.Player
 {
     public class Player : MonoBehaviour
     {
         private void Update()
         {
-            if (Input.GetMouseButton(0))
-            {
-                MoveToCursor();
-            }
+            if (ApplyCombat()) return;
+            if (ApplyMovement()) return;
+            print("nothing to do.!");
         }
-        private void MoveToCursor()
+
+        private bool ApplyCombat()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit[] hits = Physics.RaycastAll(GetRayMouse());
+            foreach (RaycastHit hit in hits)
+            {
+                TargetEnemy target = hit.transform.GetComponent<TargetEnemy>();
+                if (target == null) continue;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<EnemyFight>().Attack(target);
+                }
+                return true;
+            }
+            return false;
+        }
+
+        private bool ApplyMovement()
+        {
             RaycastHit rayHit;
-            bool hasHit = Physics.Raycast(ray, out rayHit);
+            bool hasHit = Physics.Raycast(GetRayMouse(), out rayHit);
             if (hasHit == true)
             {
-                GetComponent<Move>().MoveTo(rayHit.point);
+                if (Input.GetMouseButton(0))
+                {
+                    GetComponent<Move>().MoveTo(rayHit.point);
+                }
+                return true;
             }
+            return false;
+        }
+
+        private static Ray GetRayMouse()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 
